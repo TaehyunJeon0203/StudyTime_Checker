00be70c28ffcb1f258e68d769548a1e6ee31171d
TaehyunJeon <jeontaehyun0203@gmail.com>
2022-02-04 15:01:30 +0900

    Featdel: memo기능 불필요하다 판단해 비활성화(숨김) 코드는 남아있음

--- js/memo.js
+++ js/memo.js
@@ -4,11 +4,34 @@ const addMemoBtn = document.getElementById("addmemobutton");
 const saveMemoBtn = document.getElementById("savememobutton");
 
 addMemoBtn.addEventListener("click" ,addMemo);
+saveMemoBtn.addEventListener("click", handleMemoSubmit);
+
+function saveMemo() {
+    localStorage.setItem(MEMO_KEY, JSON.stringify(memo));
+}
 
 function addMemo() {
     const memoInput = document.createElement("input");
     memoInput.placeholder = "Please enter a note";
     memoArea.appendChild(memoInput); 
+}
+
+function paintMemo() {
+    const h1 = document.createElement("h1");
+    h1.id = "newMemo";
+    document
+
+}
+
+function handleMemoSubmit() {
+    const newMemo = memoInput.value;
+    memoInput.value = "";
+    const newMemoObj = {
+        text: newMemo,
+        id: Date.now(),
+    };
+    memo.push(newMemoObj);
+    saveMemo();
 }
 
 function enterNote() {
